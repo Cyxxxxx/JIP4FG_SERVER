fd5b4da8d4fbff929a9bcb45ecd42bb4c03501bd
yuc <732298225@qq.com>
2021-01-04 01:45:15 +0800

    update: [岗位推荐] 修改定时任务时间

--- src/main/java/cn/yuc/jip4fg_server/user/recommend/config/UserJobRecommendConfig.java
+++ src/main/java/cn/yuc/jip4fg_server/user/recommend/config/UserJobRecommendConfig.java
@@ -8,14 +8,14 @@ package cn.yuc.jip4fg_server.user.recommend.config;
 public class UserJobRecommendConfig {
 
     /**
-     * 信息推送，默认每天9点，15点，20点开始推送一次
+     * 信息推送，默认每天15点开始推送一次
      */
-    public final static String JOB_RECOMMEND_CRON = "0 0 9,15,20 * * *";
+    public final static String JOB_RECOMMEND_CRON = "0 0 15 * * *";
 
     /**
      * 默认每天0点收集一次
      */
-    public final static String USER_DEMAND_COLLECT = "0 0 0 * * * *";
+    public final static String USER_DEMAND_COLLECT = "0 0 0 * * *";
 
     /**
      * 存储于Redis中的键头，用于搜索
